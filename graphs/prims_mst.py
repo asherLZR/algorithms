1ad7b2bce062097bfa1a96cb4e54da28fd6d938f
asherLZR <asher.lzrz@gmail.com>
2018-07-18 17:28:28 +1000

    Re-comment for specificity

--- graphs/prims_mst.py
+++ graphs/prims_mst.py
@@ -19,9 +19,9 @@ C[random.choice(U)] = 0
 while U:
     # choose a vertex by the lowest cost in C
     chosen, c_cost = min(enumerate(C), key=lambda x: x[1])
-    # once it has been chosen, its cost returns to infinity
+    # once it has been chosen, its cost returns to infinity to prevent is from being chosen as a future min
     C[chosen] = float('inf')
-    # the vertex has now been chosen so it cannot be picked again
+    # the vertex has now been chosen so no longer consider returning to it when looking at future edges
     U.remove(chosen)
     mst_cost += c_cost
     # build the tree with the chosen vertex and edge if it exists
@@ -32,8 +32,9 @@ while U:
         # ensures v is the new undiscovered vertex
         if chosen == v:
             u, v = v, u
-        # if edge has chosen vertex, it is adjacent
+        # edge has chosen vertex so it is adjacent
         if chosen == u:
+            # adjacent edge is incident to an unvisited vertex and its weight is less than a previously found edge
             if v in U and w < C[v]:
                 # update cost of adjacent vertex
                 C[v] = w
