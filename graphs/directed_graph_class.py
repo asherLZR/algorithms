1cb0bd22edfae682aa1a11ca242351ef66d0e9b0
asherLZR <asher.lzrz@gmail.com>
2018-10-01 09:25:40 +1000

    Modified directed graph class

--- graphs/directed_graph_class.py
+++ graphs/directed_graph_class.py
@@ -7,13 +7,22 @@ class Edge:
     def __str__(self):
         return f"({self.u}, {self.v}, {self.w})"
 
+class Vertex:
+    def __init__(self, u):
+        self.id = u
+        self.discovered = False
+
+    def __str__(self):
+        return str(self.id)
+
 class DirectedGraph:
     def __init__(self, n):
+        self.vertices = [Vertex(x) for x in range(n)]
         self.adj_list = [[] for _ in range(n)]
 
     def add(self, u, v, w):
         # noinspection PyTypeChecker
-        self.adj_list[u].append(Edge(u, v, w))
+        self.adj_list[u].append(Edge(self.vertices[u], self.vertices[v], w))
 
     def __str__(self):
         return "\n".join([str(i) + ": " + ", ".join(str(e) for e in e_list) for i, e_list in enumerate(self.adj_list)])
